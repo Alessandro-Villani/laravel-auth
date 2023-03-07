d475d96a8cf844018ed6a35dbfff16ddb3d19c04
Alessandro-Villani <alessvill@libero.it>
2023-03-07 16:46:04 +0100

    populated projects migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserAdminSeeder::class]);
+        $this->call([UserAdminSeeder::class, ProjectSeeder::class]);
     }
 }
