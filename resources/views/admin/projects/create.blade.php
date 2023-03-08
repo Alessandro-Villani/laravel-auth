bc4b88b8c1e0e968e6a6a9adcb0e34979148019e
Alessandro-Villani <alessvill@libero.it>
2023-03-08 15:45:49 +0100

    added errors alert

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -6,15 +6,7 @@
 
 <div class="container py-5 text-center">
     <h1 class="mb-5">ADD PROJECT</h1>
-    @if ($errors->any())
-        <div class="col-12 alert mb-3 container p-3">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li class="mb-1">{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    @include('includes.alerts.errors')
     @include('includes.projects.form')
 </div>
 
